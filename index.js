c99ae595cf1c0a274c2d70790919c64617c4f224
Shamila K A <shamilaabdulkadher@gmail.com>
2025-04-05 18:17:27 +0530

    second commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const timerEl=document.getElementById("timer");
-const startEl=document.getElementById("start";
+const startEl=document.getElementById("start");
 const stopEl=document.getElementById("stop");
 const resetEl=document.getElementById("reset");
 
-let startTimer=0;
+let startTime=0;
 let elapsedTimer=0;
 let timeInterval;
 
 function startTimer(){
-    startTimer=Date.now()-elapsedTimer;
+    console.log("Hello");
+    startTime=Date.now()-elapsedTimer;
 
     timeInterval=setInterval(()=>{
-        elapsedTimer=Date.now()-startTimer;
-        timerEl.textContent=
+        elapsedTimer=Date.now()-startTime;
+        timerEl.textContent=formatTime(elapsedTimer);
     },10);
 }
 function formatTime(){
@@ -20,15 +21,27 @@ function formatTime(){
     const seconds=Math.floor(elapsedTimer%(1000*60)/1000);
     const minutes=Math.floor(elapsedTimer%(1000*60*60)/(1000*60));
     const hours=Math.floor(elapsedTimer/(1000*60*60));
-    return
+    return(
     (hours?(hours>9?hours:"0"+hours):"00")
     +":"+
     (minutes?(minutes>9?minutes:"0"+minutes):"00")
     +":"+
     (seconds?(seconds>9?seconds:"0"+seconds):"00")
     +":"+
-    (milliseconds?(milliseconds>9?milliseconds:"0"+milliseconds)
+    (milliseconds>9?milliseconds:"0"+milliseconds)
 );
 }
 
-startEl.addEventListener("click",startTimer)
+function stopTimer(){
+    clearInterval(timeInterval);
+    
+}
+
+function resetTimer(){
+clearInterval(timeInterval);
+elapsedTimer=0;
+timerEl.textContent ="00:00:00";
+}
+startEl.addEventListener("click",startTimer);
+stopEl.addEventListener("click",stopTimer);
+resetEl.addEventListener("click",resetTimer);
